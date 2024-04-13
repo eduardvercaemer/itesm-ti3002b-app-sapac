fe8b18ec35224cedb32345e189c9e5826d0066f1
ed <vercaemereduard@gmail.com>
2024-04-12 18:26:14 -0600

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,73 @@
+import { useCallback } from "react";
+import Papa from "papaparse";
+import { atom, useRecoilState } from "recoil";
+
+const employees$ = atom({
+  key: "employees",
+  default: new Map(),
+});
+
 function App() {
-  return <h1>sapac</h1>;
+  const [employees, setEmployees] = useRecoilState(employees$);
+
+  const handleDrop = useCallback(async (/**DragEvent*/ e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (!file) {
+      return;
+    }
+
+    const newEmployees = new Map();
+
+    Papa.parse(file, {
+      header: true,
+      step({ data }) {
+        const [id, name, time] = Object.values(data);
+        if (!newEmployees.has(id)) {
+          newEmployees.set(id, { name });
+        }
+
+        console.debug({ id, name, time });
+      },
+      complete() {
+        setEmployees(newEmployees);
+      },
+    });
+  }, []);
+
+  const onDragOver = useCallback((e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  }, []);
+
+  const onDragEnter = useCallback((e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  }, []);
+
+  return (
+    <main className="flex flex-col h-screen w-screen">
+      <h1 className="font-bold">sapac</h1>
+      <div
+        className="grow"
+        onDragEnter={onDragEnter}
+        onDragOver={onDragOver}
+        onDrop={handleDrop}
+      >
+        <table>
+          <tbody>
+            {Array.from(employees.entries()).map(([id, e]) => (
+              <tr>
+                <td>{id}</td>
+                <td>{e.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { RecoilRoot } from "recoil";
 
-import "normalize.css/normalize.css";
+import "./index.css";
+
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 );
