8b24b4f32a3718c811d5740eaa09d57a43b6f1b0
Brandocean <brandocean2016@gmail.com>
2024-04-16 09:44:42 -0600

    First design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { useData, useSetFile } from "./handkey-module/state.js";
 import { FileDrop } from "./components/file-drop.jsx";
 
+import "./App.css";
+
 function App() {
   const setFile = useSetFile();
   const data = useData();
@@ -10,14 +12,36 @@ function App() {
   useEffect(() => console.debug(data), [data]);
 
   return (
-    <main className="flex flex-col h-screen w-screen">
-      <h1 className="font-bold">sapac</h1>
-      <FileDrop
-        className="grow bg-violet-400 hover:cursor-pointer"
-        onFileDrop={setFile}
-      >
-        drop handkey csv in purple rect (check console for output)
-      </FileDrop>
+    <main className="blue-square">
+
+      <h1 className="title">Sube tus archivos de Excel</h1>
+
+      <div className="container">
+
+        <div className="file-drop-container">
+          <h2 className="file-drop-title">Plantilla Incidentes</h2>
+          <FileDrop
+            className="file-drop"
+            onFileDrop={setFile}
+          >
+          </FileDrop>
+        </div>
+
+        <div className="file-drop-container">
+          <h2 className="file-drop-title">Archivo Handkey</h2>
+          <FileDrop
+            className="file-drop"
+            onFileDrop={setFile}
+          >
+          </FileDrop>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center mt-12">
+        <button className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48">Iniciar</button>
+        <a className="flex justify-center underline my-4">Previsualizar</a>
+      </div>
+
     </main>
   );
 }
