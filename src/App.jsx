c8c7b34a14427fc52d6094c3ddd486d051bb942a
Brandocean <brandocean2016@gmail.com>
2024-04-16 19:57:17 -0600

    Add hover filedrop

--- src/App.jsx
+++ src/App.jsx
@@ -15,42 +15,50 @@ function App() {
   return (
     <main className="blue-square">
 
-      <h1 className="title">Sube tus archivos de Excel</h1>
+      <div className="title-container">
+        <h1 className="title">Sube tus archivos de Excel</h1>
+      </div>
 
       <div className="container">
 
         <div className="file-drop-container">
           <h2 className="file-drop-title">Plantilla Incidentes</h2>
           <FileDrop
-            className= "file-drop"
+            className="file-drop"
             onFileDrop={setFile}
           >
             <img
-              className="w-16 h-16 opacity-50"
+              className="subir-a-la-nube"
               src="../public/subir-a-la-nube.png"
               alt="subir-a-la-nube"
             />
             <p className="text-gray-400">Arrastra y suelta aquí</p>
             <p className="text-gray-400 mb-1">o</p>
-            <button className="mx-auto bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded w-48">Seleccionar archivo</button>
+            <button className="button-select-file">Seleccionar archivo</button>
           </FileDrop>
         </div>
 
         <div className="file-drop-container">
           <h2 className="file-drop-title">Archivo Handkey</h2>
           <FileDrop
-            className="file-drop-upload"
+            className="file-drop"
             onFileDrop={setFile}
           >
-            <div class="loader"></div>
-            <p className="text-white font-bold">Suelta el archivo</p>
+            <img
+              className="subir-a-la-nube"
+              src="../public/subir-a-la-nube.png"
+              alt="subir-a-la-nube"
+            />
+            <p className="text-gray-400">Arrastra y suelta aquí</p>
+            <p className="text-gray-400 mb-1">o</p>
+            <button className="button-select-file">Seleccionar archivo</button>
           </FileDrop>
         </div>
       </div>
 
-      <div className="flex flex-col justify-center mt-12">
-        <button className="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48">Iniciar</button>
-        <a className="flex justify-center underline my-4 hover:text-blue-700 cursor-pointer">Previsualizar</a>
+      <div className="bottom-container">
+        <button className="bottom-btn">Iniciar</button>
+        <a className="bottom-preview">Previsualizar</a>
         <ExportCsv />
       </div>
 
@@ -59,3 +67,11 @@ function App() {
 }
 
 export default App;
+
+{/* <FileDrop
+            className="file-drop-upload"
+            onFileDrop={setFile}
+          >
+            <div class="loader"></div>
+            <p className="text-white font-bold">Suelta el archivo</p>
+          </FileDrop> */}
