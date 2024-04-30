b61fc2fbcb2795a4461fa0056ff98a9dc8ab3a4b
Jorge E. Turner Escalante <51143651+HerrTurner@users.noreply.github.com>
2024-04-30 15:15:21 -0600

    Added Colors (#15)
    * Crear board y persona
    
    * Agregar div de periodo
    
    * Mejora Color Incidencias
    
    * Detalles
    
    * changed component to componets folder
    
    * Testing with actual data
    
    * Getting data from csv files
    
    * Next Back button testing working
    
    * table working with data
    
    * Some style
    
    * Table working according to date range
    
    * Periodo is now updated as well
    
    * Table now detects if employee has certain values
    
    * First render bug fixed
    
    * Mejora de disenio de board
    
    * Small bug with date
    
    * Some design to profile
    
    * Resize some things
    
    * Table y Person combinado
    
    * Se arreglaron botones de siguiente y anterior
    
    * Otra version del dashboard
    
    * Otra otra mejora del dashboard, botones y scroll
    
    * Format Entries testing
    
    * Saving currIndex
    
    * Agregar logica de incidencia
    
    * Implementacion basica de Modal para Incidencias
    
    * Mejoras al Modal de (scroll, map)
    
    * incidences and observations strings added
    
    * automatic incidences testing
    
    * Correccion de error de css
    
    * Small change from suarez merge
    
    * Edit incidences now working
    
    * Edit incidences working
    
    * Se utiliza el diccionario para añadir las opciones del modal
    
    * se cambio hardcodeo por datos de diccionario en merge
    
    * small bug fixed, but refactor is recommended
    
    * Funcion modificada para que utilice currIncidence como opción default al abrir el modal
    
    * Export navigate added, plus now when user deletes files it deletes the currIndex
    
    * Added colors
    
    * refresh bug solved
    
    * Cambios en diseño del Modal Incidencia
    
    * incidences updated
    
    * icons added
    
    ---------
    
    Co-authored-by: SnipperPlayer <galeana2002@hotmail.com>
    Co-authored-by: Ant20ni02 <antoniogarcort@hotmail.com>
    Co-authored-by: Roy-maker <carsanchezsie@gmail.com>
    Co-authored-by: Supercito-132 <A01423225@itesm.mx>
    Co-authored-by: Alejandro Suárez <suga.gonza.18@gmail.com>

---
+++ src/components/incidence.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react';
+import './incidence.css';
+import { useCreateIncidence, useEditIncidence } from "../handkey-module/state";
+
+export const Incidence = ({
+  onClose,
+  options,
+  currEmployeeId,
+  currDate,
+  currIncidence,
+}) => {
+  const createIncidence = useCreateIncidence();
+  const editIncidence = useEditIncidence();
+
+    const [selectedOption, setSelectedOption] = useState("");
+    
+    const optionToId = {
+        'Retardo': 'r',
+        'Falta': 'f',
+        'Día Económico': 'de',
+        'Vacaciones': 'vac',
+        'Permuta': 'perm',
+        'Incapacidad': 'inc',
+        'Justificación Entrada': 'je',
+        'Justificación salida': 'js',
+        'Retardo Leve': 'rl',
+        'Retardo Grave': 'rg',
+        'Correcto': 'ok',
+        'Permiso Sindical': 'ps',
+        'Falta Entrada': 'fe',
+        'Falta Salida': 'fs',
+        'Descanso': 'd',
+        'Lic. con goce de sueldo': 'lcgs',
+        'Lic. sin goce de sueldo': 'lsgs',
+        'Onomástico': 'ono' 
+    };
+
+    const idToOption = {
+        'r': 'Retardo',
+        'f': 'Falta',
+        'de': 'Día Económico',
+        'vac': 'Vacaciones',
+        'perm': 'Permuta',
+        'inc': 'Incapacidad',
+        'je': 'Justificación Entrada',
+        'js': 'Justificación salida',
+        'rl': 'Retardo Leve',
+        'rg': 'Retardo Grave',
+        'ok': 'Correcto',
+        'ps': 'Permiso Sindical',
+        'fe': 'Falta Entrada',
+        'fs': 'Falta Salida',
+        'd': 'Descanso',
+        'lcgs': 'Lic. con goce de sueldo',
+        'lsgs': 'Lic. sin goce de sueldo',
+        'ono': 'Onomástico'
+    };
+
+    useEffect(() => {
+        setSelectedOption(idToOption[currIncidence]);
+    }, [currIncidence]);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+    const handleSave = () => {
+        if (selectedOption) {
+            if(currIncidence === ""){
+                createIncidence(currEmployeeId, currDate, optionToId[selectedOption])
+            }
+            else{
+                editIncidence(currEmployeeId, currDate.getTime(), optionToId[selectedOption])
+            } 
+            onClose();
+        } else {
+        alert('Por favor, selecciona una opción antes de guardar.');
+        }
+    };
+
+  const handleCloseModal = () => {
+    onClose();
+  };
+
+  return (
+    <div className="modal">
+      <div className="modalContent">
+        <div className="topContent">
+          <div className="containerIncidencia">
+            <h1>Incidencia</h1>
+          </div>
+          {/* Movemos el botón X al lado izquierdo del título Incidencia */}
+          <button className="crossButton" onClick={handleCloseModal}>
+            <span className="font-bold text-xl">X</span>
+          </button>
+        </div>
+
+        <div className="optionContainer">
+          {options.map((option, index) => (
+            <div key={index} className="marginOptions">
+              <label htmlFor={option} className="labelMap">
+                <input
+                  className="incidenceOption"
+                  type="radio"
+                  id={option}
+                  value={option}
+                  checked={selectedOption === option}
+                  onChange={handleOptionChange}
+                />
+                <span className="incidenceText">{option}</span>
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <div className="containerIncidencia">
+          <button className="saveButton" onClick={handleSave}>
+            Guardar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Incidence;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,53 +1,132 @@
+import { useState , useMemo, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+import Board from '../../components/Board'
+import { useCreateIncidence, useEditIncidence, useEmployeeList, useEmployee } from "../../handkey-module/state";
+
 import "./Dashboard.css";
+import { useFetcher } from 'react-router-dom';
 
-import {
-  useCreateIncidence,
-  useEditIncidence,
-  useEmployeeList,
-  useEmployee,
-} from "../../handkey-module/state";
+const formatEntries = (employee) => {
+    const formattedEntries = [];
 
-import { useEffect, useState } from "react";
+    if(employee  && employee.days) {
+        employee.days.forEach((day) => {
+            const entryDay = day.date.getUTCDate();
+            const entryMonth = day.date.getUTCMonth() + 1;
+            const entryYear = day.date.getUTCFullYear();
+            const formattedDate = `${entryDay}-${entryMonth}-${entryYear}`;
+
+            let entry = "";
+            let exit = "";
+
+            if(day.entries[0]) {
+                const entryDate = new Date(day.entries[0]);
+                const entryHours = entryDate.getUTCHours();
+                const entryMinutes = "0" + entryDate.getUTCMinutes();
+                entry = entryHours + ':' + entryMinutes.slice(-2)
+            }
+            if(day.entries[1]){
+                const exitDate = new Date(day.entries[1]);
+                const exitHours = exitDate.getUTCHours();
+                const exitMinutes = "0" + exitDate.getUTCMinutes();
+                exit = exitHours + ':' + exitMinutes.slice(-2)
+            }
+
+            formattedEntries.push({
+                fecha: formattedDate,
+                entrada: entry,
+                salida: exit,
+                incidencia: day.incidence ? day.incidence : "" ,
+                observaciones: day.observation ? day.observation : "",
+                unformattedDate: day.date
+            });
+        });
+    }
+
+    return formattedEntries;
+}
+
+function formatTime(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours < 10 ? '0' + hours : hours}:${mins < 10 ? '0' + mins : mins}`;
+}
 
 function Dashboard() {
-  const createIncidence = useCreateIncidence();
-  const editIncidence = useEditIncidence();
-  const list = useEmployeeList();
-  const e = useEmployee("127");
-  const [ready, setReady] = useState(false);
+    const navigate = useNavigate();
+    const employees = useEmployeeList();
+    const date_from = new Date(parseInt(localStorage.getItem("state/start-date"), 10));
+    const date_to = new Date(parseInt(localStorage.getItem("state/end-date"), 10));
+    const [ currIndex, setCurrIndex ] = useState(localStorage.getItem("currIndex") !== null ? parseInt(localStorage.getItem("currIndex")) : 0);
 
-  useEffect(() => {
-    if (!e) return;
-    if (ready) return;
+    const currEmployee = useEmployee(employees[currIndex]);
+    const currEntries = useMemo(() => formatEntries(currEmployee), [currEmployee]);
+    const currEmployeeId = useMemo(() => employees[currIndex], [currIndex]);
 
-    console.log("about to infer");
-    setReady(true);
-    setTimeout(() => e.inferIncidences(), 0);
-  }, [e]);
+   useEffect(() => {
+        setTimeout(() => currEmployee.inferIncidences(), 0);
+    }, [currEmployee]);
+    
 
-  return (
-    <main>
-      <div>Dashboard</div>
-      <button
-        onClick={() => {
-          createIncidence("127", new Date(), "r");
-        }}
-      >
-        click me to add an incidence
-      </button>
-      <button
-        onClick={() => {
-          const date = e.employee.incidences[0].date;
-          editIncidence("127", date, "nr");
-        }}
-      >
-        edit incidence
-      </button>
-      <pre>
-        <code>{JSON.stringify(e, null, 2)}</code>
-      </pre>
-    </main>
-  );
+    const handleBackClick = () => {
+        console.log(currEmployee);
+        localStorage.setItem("currIndex", currIndex - 1)
+        setCurrIndex(currIndex - 1);
+    }
+    
+    const handleNextClick = () => {
+        console.log(currEmployee);
+        localStorage.setItem("currIndex", currIndex + 1)
+        setCurrIndex(currIndex + 1);
+    }
+
+    const handleExportClick = () => {
+        navigate("/preview");
+    }
+
+    if (!currEmployee || !currEmployee.employee) return <div>Cargando...</div>;
+
+    return (
+        <div className='contdash'>
+            <div className='dashboard-container'>
+                <div className='cardPerson'>
+                    <img src='\profilepic.jpg'/>
+                    <div className='employee-id'>
+                        <span className='label-id'>Número de empleado:</span> {currEmployeeId ? currEmployeeId : employees[currIndex]}
+                    </div>
+                    <div className='employee-name'>
+                        <span className='label'>Nombre:</span> {currEmployee.employee.name}
+                    </div>
+                    <div className='employee-address'>
+                        <span className='label'>Dirección:</span> {currEmployee.employee.address}
+                    </div>
+                    <div className='employee-kind'>
+                        <span className='label'>Tipo:</span> {currEmployee.employee.kind}
+                    </div>
+                    <div className='schedule-time'>
+                        <span className='label'>Horario: </span>
+                        <span>
+                        {formatTime(currEmployee.employee.schedule.start)} - {formatTime(currEmployee.employee.schedule.end)}
+                        </span>
+                    </div>
+                </div>
+
+                <div className='cardTable'>
+                    <Board objeto={currEntries} currEmployeeId={currEmployeeId} date_from={date_from} date_to={date_to}/>
+                </div>
+            </div>
+            <div className='button-container'>
+                {currIndex > 0 ?
+                    <button className='button left-button' onClick={handleBackClick}>Anterior</button> :
+                    <button className='button left-button disabled' disabled>Anterior</button>
+                }   
+                {currIndex < employees.length - 1 ? 
+                    <button className='button right-button' onClick={handleNextClick}>Siguiente</button> :
+                    <button className='button right-button' onClick={handleExportClick}>Exportar</button>    
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard;
