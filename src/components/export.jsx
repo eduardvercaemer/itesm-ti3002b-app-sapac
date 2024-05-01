30776e8c62cdaa60e2c66ef77bed8b8ae0a00fe0
Miguel Jiménez <mike0vortex@gmail.com>
2024-04-30 23:13:31 -0600

    export component updated, address filter added, worksheet styles up to date

--- src/components/export.jsx
+++ src/components/export.jsx
@@ -1,5 +1,8 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Papa from "papaparse";
+import { useEmployeeList, useEmployee } from "../handkey-module/state";
+import tEmployee from "../../testing-employee";
+import ExcelJS from 'exceljs';
 
 const df = new Intl.DateTimeFormat("es-MX", {
   timeZone: "America/Mexico_City",
@@ -9,33 +12,173 @@ const df = new Intl.DateTimeFormat("es-MX", {
 });
 
 export function ExportCsv() {
-  const rows = useMemo(() => {
-    //if (!data.entries) {
-    //  return null;
-    //}
-    //return Array.from(data.entries).flatMap(employeeToRows);
-  }, []);
+  const [addresses, setAddresses] = useState([]);
+  const [filteredData, setFilterData] = useState([]);
+  const employees = tEmployee;
 
-  const onClick = useCallback(() => {
-    if (!rows) {
+  const getAddresses = () =>{
+    // still need to update with local memo
+    let decoyArr = []
+
+    for (let x = 0; x < employees.length; x++) {
+
+      const currEmployee = employees[x];
+
+      if (!decoyArr.includes(currEmployee.employee.address)){
+        decoyArr.push(currEmployee.employee.address);
+      }
+
+    }
+    setAddresses(decoyArr);
+  }
+
+  useEffect(()=>{
+    getAddresses();
+  },[])
+
+
+  const findIndexByKey = (array, key, value) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i][key] === value) {
+        return i;
+      }
+    }
+    return -1
+  }
+
+  useEffect(()=>{
+
+    if(filteredData.length === 0){
+      for (let y = 0; y < employees.length; y++) {
+
+        let currAddress = employees[y].employee.address;
+
+        const alreadyStored = filteredData.some(e => Object.values(e).includes(currAddress));
+
+
+        if (alreadyStored) {
+          //current address already exists on array
+          const objectIndex = findIndexByKey(filteredData, 'address', currAddress);
+          const objectCopy = filteredData[objectIndex];
+          const employeesCopy = objectCopy.employees;
+          employeesCopy.push(employees[y]);
+
+          const newObject = { address: currAddress, employees: employeesCopy };
+          const fullFilterDataCopy = filteredData;
+          fullFilterDataCopy[objectIndex] = newObject;
+
+          setFilterData(fullFilterDataCopy);
+
+        }
+        else {
+          const newObject = { address: currAddress, employees: [employees[y]] };
+          const fullFilterDataCopy = filteredData;
+          fullFilterDataCopy.push(newObject);
+
+          setFilterData(fullFilterDataCopy);
+        }
+
+      }
+    }
+
+  },[addresses])
+
+  function numberToLetter(number) {
+    let letter = '';
+    while (number > 0) {
+      const remainder = (number - 1) % 26;
+      letter = String.fromCharCode(65 + remainder) + letter;
+      number = Math.floor((number - remainder) / 26);
+    }
+    return letter;
+  }
+
+  function letterToNumber(letter) {
+    return letter.charCodeAt(0) - 64;
+  }
+
+
+
+  //start = 'C'
+  //s = 0;
+  //let letter = letterToNumber(start);
+  //let current = numberToLetter(letter + s);
+
+  const fillDays = (worksheet, start, end, rowIndex) =>{
+    let letter = letterToNumber(start);
+    let current = 0;
+    const delta = letterToNumber(end) - letterToNumber(start);
+    console.log(delta);
+
+    for(let s = 0; s <= delta; s++){
+      current = numberToLetter(letter + s);
+
+      worksheet.getCell(`${current}+${rowIndex}`).fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FFFF00' }
+      };;
+
+    }
+
+  } 
+
+
+
+
+
+  const onClick = async() => {
+
+    if (!filteredData) {
       return;
     }
 
-    const csv = Papa.unparse({
-      fields: ["ID", "Nombre", "Tiempo"],
-      data: rows,
-    });
+    const workbook = new ExcelJS.Workbook();
+    for (let p = 0; p < addresses.length; p++) {
 
-    const blob = new Blob([csv], { type: "text/csv" });
-    const url = URL.createObjectURL(blob);
+      // create a worksheet for every address
+      const currWorksheet = workbook.addWorksheet(addresses[p]);
 
+      currWorksheet.getCell('A1').value = "ID";
+      currWorksheet.getCell('B1').value = "Nombre";
+      currWorksheet.mergeCells('C1:Q1');
+      currWorksheet.getCell('C1').value = "Días";
+      currWorksheet.getCell('R1').value = "Observaciones";
+
+
+      let rowIndex = 2;
+      for (let z = 0; z < filteredData[p].employees.length; z++) {
+        //fill rows
+        currWorksheet.getCell(`A${ rowIndex }`).value = z + 1;
+        currWorksheet.getCell(`B${ rowIndex }`).value = filteredData[p].employees[z].employee.name;
+        /* currWorksheet.getCell(`C${rowIndex}:Q${rowIndex}`).fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: 'FFFF00' }
+        }; */
+        fillDays(currWorksheet,'C','Q',rowIndex);
+
+        currWorksheet.getCell(`R${ rowIndex }`).value = filteredData[p].employees[z].employee.observations;
+        rowIndex++;
+      }
+
+   
+    } 
+
+    // Generar y descargar el archivo Excel
+    const buffer = await workbook.xlsx.writeBuffer();
+    const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const url = window.URL.createObjectURL(blob);
     const anchor = document.createElement("a");
     anchor.setAttribute("href", url);
-    anchor.setAttribute("download", "handkey-limpio.csv");
+    anchor.setAttribute("download", "handkey-limpio.xlsx");
     anchor.setAttribute("style", "display:none");
     document.body.appendChild(anchor);
     anchor.click();
-  }, [rows]);
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(anchor);
+
+  };
 
   return <button onClick={onClick}>export csv</button>;
 }
