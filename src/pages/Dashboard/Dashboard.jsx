474d5e7e785ebdfd3493cbd28252f29b3b2ecd8a
Roy-maker <carsanchezsie@gmail.com>
2024-05-06 18:41:49 -0600

    Searchbar and button

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import formatEntries from "../../handkey-module/formatEntries";
 
 import { useEmployeeList, useEmployee } from "../../handkey-module/state";
 import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import "./Dashboard.css";
 
@@ -80,39 +82,64 @@ function Dashboard() {
     navigate("/preview");
   };
 
+  const handleSearchClick = () => {
+    const searchTerm = document.querySelector(".search-bar").value;
+    console.log(searchTerm);
+  };
+
   if (!currEmployee || !currEmployee.employee) return <div>Cargando...</div>;
 
   return (
     <div className="h-screen w-screen flex flex-col py-10">
       <div className="grow flex gap-14 px-14 overflow-hidden">
-        <div className="cardPerson">
-          <div className="cardPersonContent">
-            <div className="employee-id">
-              <span className="label-id">Número de empleado:</span>
-              <br /> {currEmployeeId ? currEmployeeId : employees[currIndex]}
-            </div>
-            <div className="employee-name">
-              <span className="label">Nombre:</span>{" "}
-              {currEmployee.employee.name}
-            </div>
-            <div className="employee-address">
-              <span className="label">Dirección:</span>{" "}
-              {currEmployee.employee.address}
-            </div>
-            <div className="employee-kind">
-              <span className="label">Tipo:</span> {currEmployee.employee.kind}
-            </div>
-            {currEmployee.employee.schedule && (
-              <div className="schedule-time">
-                <span className="label">Horario: </span>
-                <span>
-                  {formatTime(currEmployee.employee.schedule.start)} -{" "}
-                  {formatTime(currEmployee.employee.schedule.end)}
-                </span>
-              </div>
-            )}
+        <div>
+          <div className="search-container">
+          <input 
+            type="number" 
+            className="search-bar" 
+            placeholder="Buscar por id..." 
+            min="0"
+            onKeyDown={(e) => {
+                const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete', 'Home', 'End'];
+                if (!allowedKeys.includes(e.key) && isNaN(parseInt(e.key, 10))) {
+                  e.preventDefault();
+                }
+              }}
+            />
+            <button className="search-button" onClick={handleSearchClick}>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
           </div>
-          <img className="sapacLogo" src="/sapac-logo.png" />
+          <div className="cardPerson">
+            <div className="cardPersonContent">
+              <div className="employee-id">
+                <span className="label-id">Número de empleado:</span>
+                <br /> {currEmployeeId ? currEmployeeId : employees[currIndex]}
+              </div>
+              <div className="employee-name">
+                <span className="label">Nombre:</span>{" "}
+                {currEmployee.employee.name}
+              </div>
+              <div className="employee-address">
+                <span className="label">Dirección:</span>{" "}
+                {currEmployee.employee.address}
+              </div>
+              <div className="employee-kind">
+                <span className="label">Tipo:</span>{" "}
+                {currEmployee.employee.kind}
+              </div>
+              {currEmployee.employee.schedule && (
+                <div className="schedule-time">
+                  <span className="label">Horario: </span>
+                  <span>
+                    {formatTime(currEmployee.employee.schedule.start)} -{" "}
+                    {formatTime(currEmployee.employee.schedule.end)}
+                  </span>
+                </div>
+              )}
+            </div>
+            <img className="sapacLogo" src="/sapac-logo.png" />
+          </div>
         </div>
 
         <div className="grow overflow-hidden flex flex-col gap-4">
